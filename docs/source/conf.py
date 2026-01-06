6ec611e71988179867930f9baf94596380207549
hengxt <hengxt577@163.com>
2026-01-06 18:20:46 +0800

    docs: 更新文档配置并添加CI工作流
    设置默认语言为英语，更新intersphinx映射配置
    添加GitHub Actions工作流用于构建和部署文档

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -208,7 +208,7 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'https://docs.python.org/': None,
-    'https://docs.scipy.org/doc/numpy/': None,
-    'https://matplotlib.org': None
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None)
 }
